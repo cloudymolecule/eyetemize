2acd4515a067e70218bf5af1794cf195f57751d9
Cloudymolecule <baires82@gmail.com>
2021-10-20 12:57:44 -0400

    Add React Router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,26 @@
 import '../styles/App.css'
+import  { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Title from '../components/Title'
 import Menu from './Menu'
 
 function App() {
   return (
-    <div className='outer-container'>
-      <div className='inner-container'>
-        <Title />
-        <Menu />
-        <Footer />
+    <Router>
+      <div className='outer-container'>
+        <div className='inner-container'>
+          <Title />
+          <Menu />
+          <Switch>
+            <Route path='/new' exact component={New} />
+            <Route path='/notes' exact component={Notes} />
+            <Route path='/about' exact component={About} />
+          </Switch>
+          <Footer />
+
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -4,10 +4,6 @@ import '../styles/Menu.css'
 function Menu() {
     return (
         <div className='menu'>
-            {/* <button className='button'>NEW</button>
-            <button className='button'>NOTES</button>
-            <button className='button'>ABOUT </button> */}
-
             <a href="#" className='button'>NEW</a>
             <a href="#" className='button'>NOTES</a>
             <a href="#" className='button'>ABOUT</a>
