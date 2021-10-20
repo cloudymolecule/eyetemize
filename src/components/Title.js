d860d552f065d19e81d0e803c16edba15ff0dcf0
Cloudymolecule <baires82@gmail.com>
2021-10-20 12:00:52 -0400

    Add Title component

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Title.css'
+
+function Title() {
+    return (
+        <h1 className='title'>EYETEMIZE</h1>
+    )
+}
+
+export default Title
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import '../styles/App.css'
 import Footer from '../components/Footer'
+import Title from '../components/Title'
 
 function App() {
   return (
     <div className='outer-container'>
       <div className='inner-container'>
+        <Title />
         <Footer />
       </div>
     </div>
