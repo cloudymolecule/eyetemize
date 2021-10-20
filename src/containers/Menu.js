d5c48497465aa84386078fcb4c0631df1164e640
Cloudymolecule <baires82@gmail.com>
2021-10-20 12:41:33 -0400

    Add buttons and css

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -4,7 +4,13 @@ import '../styles/Menu.css'
 function Menu() {
     return (
         <div className='menu'>
-            menu
+            {/* <button className='button'>NEW</button>
+            <button className='button'>NOTES</button>
+            <button className='button'>ABOUT </button> */}
+
+            <a href="#" className='button'>NEW</a>
+            <a href="#" className='button'>NOTES</a>
+            <a href="#" className='button'>ABOUT</a>
         </div>
     )
 }
