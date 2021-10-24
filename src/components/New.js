32534ef4048653c705a8cd7c9686368e2c42a9fb
Dario Carlino <baires82@gmail.com>
2021-10-24 10:46:52 -0400

    Update New.js and css

--- src/components/New.js
+++ src/components/New.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../styles/New.css'
+import { Link } from 'react-router-dom'
 
 function New() {
     return (
-        <div>
-            new
+        <div className='new-container'>
+            <input className='new-input' placeholder='Title'/>
+            <textarea className='new-textarea' placeholder='Type your note'/>
+            <div className='new-buttons'>
+                <Link to='/new' className='button'>SAVE</Link>
+                <Link to='/new' className='button'>RESET</Link>
+            </div>
         </div>
     )
 }
