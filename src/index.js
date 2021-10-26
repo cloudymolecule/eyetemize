cd29d851044b9cfe9639c53d5bd744a379ec1251
Dario Carlino <baires82@gmail.com>
2021-10-25 23:01:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import combineReducers from './reducers/index'
 
 const middleware = [thunk]
 
-let store = createStore()
+let store = createStore(combineReducers, composeWithDevTools(
+  applyMiddleware(...middleware)
+))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 )
