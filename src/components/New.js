452eba124eddda040a32c9cb30044d196520f99a
Cloudymolecule <baires82@gmail.com>
2021-10-26 14:23:57 -0400

    Update New.js

--- src/components/New.js
+++ src/components/New.js
@@ -1,9 +1,7 @@
-import React from 'react'
 import '../styles/New.css'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { postNote } from '../actions/notes'
-
 import React, { Component } from 'react'
 
 class New extends Component {
@@ -21,22 +19,31 @@ class New extends Component {
 
     handleSubmit = e => {
         e.preventDefault()
-        const formData = new FormData()
-        formData.append(this.state.title)
-        formData.append(this.state.content)
-        this.props.postNote(formData)
+        console.log('submitted form')
+        // const formData = new FormData()
+        // formData.append(this.state.title)
+        // formData.append(this.state.content)
+        // this.props.postNote(formData)
+    }
+
+    handleReset = e => {
+        e.preventDefault()
+        this.setState({
+            title:'',
+            content:''
+        })
     }
 
     render() {
         return (
-            <div className='new-container'>
+            <form className='new-form' onSubmit={this.handleSubmit}>
                 <input name='title' onChange={this.handleChange} className='new-input' placeholder='Title' value={this.state.title}/>
-                <textarea name='content' onChange={this.handleChange} className='new-textarea' placeholder='Type your note' value={this.state.title}/>
+                <textarea name='content' onChange={this.handleChange} className='new-textarea' placeholder='Type your note' value={this.state.content}/>
                 <div className='new-buttons'>
-                    <Link to='/new' className='button'>SAVE</Link>
-                    <Link to='/new' className='button'>RESET</Link>
+                    <button type='submit' className='button'>SAVE</button>
+                    <button onClick={this.handleReset} className='button'>RESET</button>
                 </div>
-            </div>
+            </form>
         )
     }
 }
