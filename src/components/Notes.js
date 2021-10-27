42d4d6f7520ec1d1f0187245c5db9ed53cec7e92
Dario Carlino <baires82@gmail.com>
2021-10-26 22:26:48 -0400

    Update Notes.js and css

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,8 +3,20 @@ import '../styles/Notes.css'
 
 function Notes() {
     return (
-        <div>
-            notes
+        <div className='notes-container'>
+            <div className='notes-note-title'>LIST</div>
+            <div className='notes-note-content'>NOTE</div>
+            <div className='notes-list'>
+                test
+            </div>
+            <div className='notes-content'>
+                test
+            </div>
+            {/* <br /> */}
+            {/* <div className='notes-buttons'> */}
+                    <button className='button'>EDIT</button>
+                    <button className='button'>DELETE</button>
+            {/* </div> */}
         </div>
     )
 }
